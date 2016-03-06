ea78102ca7ff1a3e4986a6b0b01b8627ab7bb900
James <jamesstanford.wdi@gmail.com>
2016-03-06 15:16:38 +1100

    adding server name test to errors/error_php.php so no errors show in live server.

--- application/errors/error_php.php
+++ application/errors/error_php.php
@@ -1,3 +1,4 @@
+<?php if($_SERVER['SERVER_NAME'] == "localhost"){ ?>
 <div id="d<?php echo $line; ?>" style="z-index:20; position: absolute; bottom: 0px; left: 0px; display: fixed; width: 95%; border:1px solid #990000; background-color: yellow; padding: 5px;">
 <span style="float: right;"><a href="javascript:{}" onclick="document.getElementById('d<?php echo $line; ?>').style.display = 'none';">[ Hide ]</a></span>
 <h4>A PHP Error was encountered</h4>
@@ -8,3 +9,4 @@ Filename: <?php echo $filepath; ?><br />
 Line Number: <?php echo $line; ?><br />
 
 </div>
+<?php } ?>
