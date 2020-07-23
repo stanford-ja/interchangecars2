fd38158ccbe394091c8dbf9176785b5655bea02c
James Stanford <james@stanfordhosting.net>
2020-07-23 17:23:30 +1000

    Adding System Map to Railroad Listing and View

--- application/controllers/rr.php
+++ application/controllers/rr.php
@@ -111,7 +111,7 @@ class Rr extends CI_Controller {
 							<a href=\"javascript:{}\" onclick=\"window.open('".WEB_ROOT.$tmp[0]."','','width=500,height=500');\">Click to view PDF</a>";
 					}else{
 						$this->dat['data'][$i]['rr_name'] .= "<a href=\"javascript:{}\" onclick=\"window.open('".WEB_ROOT.$tmp[0]."','','width=500,height=500');\">
-							<img src=\"".WEB_ROOT.$tmp[0]."\" style=\"max-width: 1200px; max-height: 1200px;\" />
+							<img src=\"".WEB_ROOT.$tmp[0]."\" style=\"max-width: 700px; max-height: 700px;\" />
 							</a>";
 					}
 
